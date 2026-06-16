9c60d63d1f92eb4f8502ba86817c21796ad9a40a
GeorgiPlPetkov <georgiplpetkov@gmail.com>
2026-06-16 17:43:18 +0300

    [mx] forgor astvisitor lol

--- include/mcastvisitor.h
+++ include/mcastvisitor.h
@@ -4,8 +4,8 @@
 #include "mcheap.h"
 
 typedef struct ASTVisitor {
-    i8 (*init)  (void* ctx, heap_header* root);
-    i8 (*visit) (void* ctx, heap_header* node, u32 depth);
+    i8 (*init)  (void* ctx, HeapHeader* root);
+    i8 (*visit) (void* ctx, HeapHeader* node, u32 depth);
     i8 (*deinit)(void* ctx);
     void* ctx;
 } ASTVisitor;
